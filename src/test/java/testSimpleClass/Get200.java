67cdef796f7feb0e0c90c55730ae9a477729045c
Anastasia SAMARTEVA <samartevan28@gmail.com>
2024-09-29 19:39:33 +0300

    Added assertion of StatusCode

---
+++ src/test/java/testSimpleClass/Get200.java
@@ -0,0 +1,49 @@
+package testSimpleClass;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+
+public class Get200 {
+
+    CloseableHttpClient client;
+
+    @BeforeMethod
+    public void setUp(){
+        client = HttpClientBuilder.create().build();
+    }
+
+    @AfterMethod
+    public void tearDown() throws IOException{
+            client.close();
+    }
+    @DataProvider(name = "endPointsData")
+    public Object[] apiData(){
+        return new Object[]{
+                PropertyReader.getProperty("githubApi"),
+                PropertyReader.getProperty("rate_limit_url"),
+                PropertyReader.getProperty("public_gists_url")
+        };
+    }
+
+    @Test(dataProvider = "endPointsData")
+    public void statusCodeTest(String endPoint) throws IOException {
+
+        HttpGet request = new HttpGet(endPoint);
+        HttpResponse response = client.execute(request);
+        int actualStatusCode = response.getStatusLine().getStatusCode();
+
+        Assert.assertEquals(actualStatusCode, 200);
+    }
+
+
+
+
+}
